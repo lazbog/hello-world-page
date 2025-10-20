ecfd536e162966c58398f3cfc7f6e5b64ac8dae7
Bogdan <10440536+lazbog@users.noreply.github.com>
2025-10-20 23:14:10 +0200

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Heading from '@/components/ui/heading';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <Heading>Hello World</Heading>
+    </main>
+  );
+}
